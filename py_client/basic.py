2c25a727b2050b913c5b701ef5c9f5ec848540c2
luanpoppe <luanpoppe@gmail.com>
2024-07-01 11:40:45 -0300

    initial commit

---
+++ py_client/basic.py
@@ -0,0 +1,10 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
+
+get_response = requests.get(endpoint, json={"query": "Hello World"})
+print(get_response.text)
+# print(get_response.json())
+print(get_response.status_code)
