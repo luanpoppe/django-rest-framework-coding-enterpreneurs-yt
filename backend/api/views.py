c692c74a8be6730cd987e03ab6e7f52843ebdb8d
luanpoppe <luanpoppe@gmail.com>
2024-07-01 13:18:54 -0300

    criado o app e Model de products

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,15 @@
+import json
 from django.http import JsonResponse
 
-def api_home(request):
-    return JsonResponse({"message": "Hi there,"})
+def api_home(request, *args):
+    body = request.body
+
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    # print(data)
+    print(request.GET)
+    return JsonResponse({"message": "Hi there,"}) 
